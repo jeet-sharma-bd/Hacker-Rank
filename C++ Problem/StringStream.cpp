db95cea37b2baf3e6373009f413a44e9ed7ba134
Sree Prosenzit Kumar Sharma <79397907+jeet-sharma-bd@users.noreply.github.com>
2021-02-22 02:00:18 -0800

    Add files via upload

---
+++ C++ Problem/StringStream.cpp
@@ -0,0 +1,18 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    string str;
+    cin >> str;
+    for(int i=0; i< str.size();i++){
+            if(str[i]== ',')
+                    cout<<endl;            
+            else cout<<str[i];
+            
+    }
+
+    return 0;
+}
