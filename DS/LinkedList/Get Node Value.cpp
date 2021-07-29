d3f0cd7e6274c8dddcf5af027be400ee6c32f41f
Sree Prosenzit Kumar Sharma <79397907+jeet-sharma-bd@users.noreply.github.com>
2021-07-29 18:53:10 +0600

    Update Get Node Value.cpp

--- DS/LinkedList/Get Node Value.cpp
+++ DS/LinkedList/Get Node Value.cpp
@@ -1,38 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 struct Node{
-	int data;
-	struct Node* next;
+    int data;
+    struct Node* next;
 };
 
 void display(struct Node* head, int p){
-	
-		 if(head->next!= NULL ){
-				int i=1;
-			while(i != p){
-				head=head->next;
-				i++;
-			}
-		
-		}
-			cout<<head->data<<endl;
+    
+         if(head->next!= NULL ){
+                int i=1;
+            while(i != p){
+                head=head->next;
+                i++;
+            }
+        
+        }
+            cout<<head->data<<endl;
 }
 
 void createNode(Node* head, int t){
-	int a;
-	cin>>a;
-	head->data=a;
-	head->next=NULL;		
-		
-		for(int i=2;i<=t;i++){
-			struct Node* ptr=new Node();
-			cin>>a;
-			ptr->data=a;
-			ptr->next=NULL;
-			
-			head->next=ptr;
-			head=head->next;
-		}
+    int a;
+    cin>>a;
+        
+    head->data=a;
+    head->next=NULL;        
+        
+        for(int i=2;i<=t;i++){
+            struct Node* ptr=new Node();
+            cin>>a;
+            ptr->data=a;
+            ptr->next=NULL;
+            
+            head->next=ptr;
+            head=head->next;
+        }
 }
 
 int main(){
@@ -43,14 +44,19 @@ cin>>x;
 for(int i=0;i<x;i++){
 struct Node* head=new Node();
 int p;
-	cin>>t;
-	createNode(head,t);
-	cin>>p;
-	display(head,abs(t-p));
-	
-	         
+    cin>>t;
+    createNode(head,t);
+    cin>>p;
+    display(head,abs(t-p));
+    
+             
 
 }
 
+
+
+
+    
+    
 
 }
